2e9baf67814a001678f81e3e854b37299758cf39
ElChicoEevee <kirbyyeray@hotmail.com>
2023-02-27 03:40:24 +0100

    Shop UNITE added

---
+++ shop/data.js
@@ -0,0 +1,410 @@
+const badges=[
+{
+        "Name": "Fashionable Style: Blastoise",
+        "Info": {
+                "Discounted Gems Price": "200",
+                "Discount Start": "01-04-2023 00:00:00 UTC",
+                "Discount End": "02-04-2023 23:59:59 UTC",
+            },
+        "type": ["B01_1006000904_2b05ebba69ae1ddb"
+   ] }, {
+        "Name": "Masked Style: Gengar",
+        "Info": {
+                "Discounted Gems Price": "720",
+                "Discount Start": "01-04-2023 00:00:00 UTC",
+                "Discount End": "02-04-2023 23:59:59 UTC",
+            },
+        "type": ["B02_1006009403_c2fdb6a999b763d4"
+   ] }, {
+        "Name": "Theater Style: Alolan Ninetales",
+        "Info": {
+                "Discounted Gems Price": "630",
+                "Discount Start": "04-03-2023 00:00:00 UTC",
+                "Discount End": "05-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_ShareGet_Ninetales_05_21c641620fa595e5"
+   ] }, {
+        "Name": "Masquerade Style: Venusaur",
+        "Info": {
+                "Discounted Gems Price": "200",
+                "Discount Start": "04-03-2023 00:00:00 UTC",
+                "Discount End": "05-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_ShareGet_Venusaur_05_2daa1b8b739fb4b5"
+   ] }, {
+        "Name": "Pastel Style: Eldegoss",
+        "Info": {
+                "Release Date": "30-03-2023 00:00:00 UTC",
+                "Available Until": "30-04-2023 23:59:59 UTC",
+                "Gems": "1200",
+            },
+        "type": ["t_ShareGet_Eldegoss_07_b081b7bf225b760b"
+   ] }, {
+        "Name": "Pastel Style: Cinderace",
+        "Info": {
+                "Release Date": "30-03-2023 00:00:00 UTC",
+                "Available Until": "30-04-2023 23:59:59 UTC",
+                "Gems": "1200",
+            },
+        "type": ["t_ShareGet_Cinderace_05_a3c1ab8403d9c208"
+   ] }, {
+        "Name": "Trainer Battle Ticket",
+        "Info": {
+                "Release Date": "21-09-2023 00:00:00 UTC",
+                "Available Until": "20-10-2023 23:59:59 UTC",
+                "Coins": "200",
+            },
+        "type": ["t_common_icon_090201_TCTicket_54e0c9c52bfc50d9"
+   ] }, {
+        "Name": "Trainer Battle Ticket",
+        "Info": {
+                "Release Date": "21-04-2023 00:00:00 UTC",
+                "Available Until": "20-05-2023 23:59:59 UTC",
+                "Coins": "200",
+            },
+        "type": ["t_common_icon_090201_TCTicket_54e0c9c52bfc50d9"
+   ] }, {
+        "Name": "Hair Dye",
+        "Info": {
+                "Release Date": "01-03-2020 00:00:00 UTC",
+                "Available Until": "01-04-2024 00:00:00 UTC",
+                "AeosTickets": "100",
+            },
+        "type": ["t_common_icon_colouring_agent_09_230421_dfc1727c7899afbe"
+   ] }, {
+        "Name": "Colored Contact Lenses",
+        "Info": {
+                "Release Date": "01-03-2020 00:00:00 UTC",
+                "Available Until": "01-04-2024 00:00:00 UTC",
+                "AeosTickets": "100",
+            },
+        "type": ["t_common_icon_pupil_09_230303_e0d03b72807aee83"
+   ] }, {
+        "Name": "Unite License: Zacian",
+        "Info": {
+                "Release Date": "26-03-2023 00:00:00 UTC",
+                "Coins": "15000",
+                "Gems": "700",
+            },
+        "type": ["t_HeroSelection_Zacian_7fd7f6706da8c714"
+   ] }, {
+        "Name": "Unite License: Goodra",
+        "Info": {
+                "Release Date": "16-03-2023 07:00:00 UTC",
+                "Available Until": "22-03-2023 23:59:59 UTC",
+                "Gems": "575",
+                "1 Week After Release": "23-03-2023 00:00:00 UTC",
+                "Coins": "12000",
+                "Gems": "575",
+            },
+        "type": ["t_HeroSelection_Goodra_4abea2317a4fb03b"
+   ] }, {
+        "Name": "Unite License: Lapras",
+        "Info": {
+                "Release Date": "13-04-2023 07:00:00 UTC",
+                "Available Until": "19-04-2023 23:59:59 UTC",
+                "Gems": "575",
+                "1 Week After Release": "20-04-2023 00:00:00 UTC",
+                "Coins": "12000",
+                "Gems": "575",
+            },
+        "type": ["t_HeroSelection_Lapras_5ef0b4df20958b99"
+   ] }, {
+        "Name": "Pastel Set (Wigglytuff)",
+        "Info": {
+                "Release Date": "23-03-2023 00:00:00 UTC",
+                "Available Until": "30-04-2023 23:59:59 UTC",
+                "Gems": "1599",
+                "Membership Gems Price": "1439",
+            },
+        "type": ["t_Banner_Suit_female_288_9f9b76f989bf206b"
+   ] }, {
+        "Name": "Performer Hat",
+        "Info": {
+                "Release Date": "06-04-2023 00:00:00 UTC",
+                "Gems": "77",
+                "AeosTickets": "770",
+                "Membership AeosTickets Price": "693",
+                "Membership Gems Price": "69",
+            },
+        "type": ["t_Hat_female_527_c27d81463267eb17"
+   ] }, {
+        "Name": "Mr. Mime Set",
+        "Info": {
+                "Release Date": "07-03-2023 00:00:00 UTC",
+                "Gems": "1599",
+                "Membership Gems Price": "1439",
+            },
+        "type": ["t_Banner_Suit_female_172_bf3013ce06d4ad06"
+   ] }, {
+        "Name": "Pokémon Face Backpack (Mr. Mime)",
+        "Info": {
+                "Release Date": "07-03-2023 00:00:00 UTC",
+                "Gems": "406",
+                "AeosTickets": "4060",
+                "Membership AeosTickets Price": "3654",
+                "Membership Gems Price": "365",
+            },
+        "type": ["t_Bag_male_495_9d1358a4664a06c5"
+   ] }, {
+        "Name": "Performer Style: Sableye",
+        "Info": {
+                "Release Date": "06-04-2023 00:00:00 UTC",
+                "Gems": "2199",
+            },
+        "type": ["t_ShareGet_Sableye_04_d67df9841784390e"
+   ] }, {
+        "Name": "Elegant Style: Clefable",
+        "Info": {
+                "Release Date": "09-03-2023 00:00:00 UTC",
+                "Gems": "2199",
+            },
+        "type": ["t_ShareGet_Clefable_04_5ef55566267637c5"
+   ] }, {
+        "Name": "Performer Style: Lapras",
+        "Info": {
+                "Release Date": "13-04-2023 07:00:00 UTC",
+                "Gems": "1050",
+            },
+        "type": ["t_ShareGet_Lapras_01_8bc3b29b2061f7dc"
+   ] }, {
+        "Name": "Performer Style: Goodra",
+        "Info": {
+                "Release Date": "18-04-2023 00:00:00 UTC",
+                "Gems": "1050",
+            },
+        "type": ["t_ShareGet_Goodra_01_8bd1736bcd32aa60"
+   ] }, {
+        "Name": "Orange Unite Style: Goodra",
+        "Info": {
+                "Release Date": "16-03-2023 07:00:00 UTC",
+                "Gems": "350",
+            },
+        "type": ["t_ShareGet_Goodra_03_c69a338bb258829f"
+   ] }, {
+        "Name": "Purple Unite Style: Goodra",
+        "Info": {
+                "Release Date": "16-03-2023 07:00:00 UTC",
+                "Gems": "350",
+            },
+        "type": ["t_ShareGet_Goodra_02_89cb9f6f43ff4cad"
+   ] }, {
+        "Name": "Red Unite Style: Duraludon",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Gems": "350",
+            },
+        "type": ["t_ShareGet_Duraludon_06_6b12830d1193176f"
+   ] }, {
+        "Name": "Blue Unite Style (Active): Eldegoss",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Gems": "350",
+            },
+        "type": ["t_ShareGet_Eldegoss_14_bf1d08e9d6a645b6"
+   ] }, {
+        "Name": "Blue Unite Style (Sporty):Gardevoir",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Gems": "350",
+            },
+        "type": ["t_ShareGet_Gardevoir_11_0e6c839fcc23803c"
+   ] }, {
+        "Name": "Pink Unite Style: Snorlax",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Gems": "350",
+            },
+        "type": ["t_ShareGet_Snorlax_16_1cbad35e197e1995"
+   ] }, {
+        "Name": "Red Unite Style: Charizard",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Gems": "350",
+            },
+        "type": ["t_ShareGet_Charizard_14_824c70f3dba73380"
+   ] }, {
+        "Name": "Pastel Style: Wigglytuff",
+        "Info": {
+                "Release Date": "27-10-2022 07:00:00 UTC",
+                "Available Until": "16-11-2022 23:59:59 UTC",
+                "Gems": "500",
+            },
+        "type": ["t_ShareGet_Wigglytuff_04_7dc3271393243aba"
+   ] }, {
+        "Name": "Purple Unite Style: Goodra + License",
+        "Info": {
+                "Release Date": "16-03-2023 07:00:00 UTC",
+                "Available Until": "22-03-2023 23:59:59 UTC",
+                "Gems": "925",
+                "Discounted Gems Price": "786",
+                "Discount Start": "16-03-2023 07:00:00 UTC",
+                "Discount End": "22-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0902_8b10049700a2578a"
+   ] }, {
+        "Name": "Orange Unite Style: Goodra + License",
+        "Info": {
+                "Release Date": "16-03-2023 07:00:00 UTC",
+                "Available Until": "22-03-2023 23:59:59 UTC",
+                "Gems": "925",
+                "Discounted Gems Price": "786",
+                "Discount Start": "16-03-2023 07:00:00 UTC",
+                "Discount End": "22-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0901_99996bdb33a7f636"
+   ] }, {
+        "Name": "Pastel Style: Wigglytuff + License",
+        "Info": {
+                "Release Date": "23-03-2023 00:00:00 UTC",
+                "Available Until": "30-04-2023 23:59:59 UTC",
+                "Gems": "816",
+            },
+        "type": ["t_combination_shop_pro_b0903_a74b710bb51c7b86"
+   ] }, {
+        "Name": "Performer Style: Lapras + License",
+        "Info": {
+                "Release Date": "13-04-2023 07:00:00 UTC",
+                "Available Until": "19-04-2023 23:59:59 UTC",
+                "Gems": "1625",
+                "Discounted Gems Price": "1381",
+                "Discount Start": "13-04-2023 07:00:00 UTC",
+                "Discount End": "19-04-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0904_a094bdde81592ffe"
+   ] }, {
+        "Name": "Ruins Style: Lucario + License",
+        "Info": {
+                "Release Date": "03-04-2023 00:00:00 UTC",
+                "Available Until": "12-04-2023 23:59:59 UTC",
+                "Gems": "2774",
+                "Discounted Gems Price": "2358",
+                "Discount Start": "03-04-2023 00:00:00 UTC",
+                "Discount End": "12-04-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0905_01cc6e275a85e155"
+   ] }, {
+        "Name": "Pastel Set (Wigglytuff) + License",
+        "Info": {
+                "Release Date": "23-03-2023 00:00:00 UTC",
+                "Available Until": "05-04-2023 23:59:59 UTC",
+                "Gems": "2059",
+                "Discounted Gems Price": "1750",
+                "Discount Start": "23-03-2023 00:00:00 UTC",
+                "Discount End": "05-04-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0906_d2a904b985573a87"
+   ] }, {
+        "Name": "Slick Spoon",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Coins": "2000",
+                "AeosTickets": "1250",
+            },
+        "type": ["t_common_icon_helditem_SlickSpoon"
+   ] }, {
+        "Name": "Rescue Hood",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Coins": "2000",
+                "AeosTickets": "1250",
+            },
+        "type": ["t_common_icon_helditem_RescueHood"
+   ] }, {
+        "Name": "Special Style: Zacian",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Gems": "1050",
+            },
+		"type": ["t_ShareGet_Zacian_01_d2b2f4eb5cbfa93e"
+        
+   ] }, {
+        "Name": "Performer Style: Sableye + License",
+        "Info": {
+                "Release Date": "06-04-2023 00:00:00 UTC",
+                "Available Until": "12-04-2023 23:59:59 UTC",
+                "Gems": "2774",
+                "Discounted Gems Price": "2358",
+                "Discount Start": "06-04-2023 00:00:00 UTC",
+                "Discount End": "12-04-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0907_d762882ef26bd495"
+   ] }, {
+        "Name": "Mr.Mime Set + Unite License",
+        "Info": {
+                "Release Date": "07-03-2023 00:00:00 UTC",
+                "Available Until": "16-03-2023 23:59:59 UTC",
+                "Gems": "2059",
+                "Discounted Gems Price": "1750",
+                "Discount Start": "07-03-2023 00:00:00 UTC",
+                "Discount End": "16-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0908_3f300184f10b1c49"
+   ] }, {
+        "Name": "Pokémon Face Backpack (Mr. Mime) + License",
+        "Info": {
+                "Release Date": "07-03-2023 00:00:00 UTC",
+                "Available Until": "16-03-2023 23:59:59 UTC",
+                "Gems": "866",
+                "Discounted Gems Price": "736",
+                "Discount Start": "07-03-2023 00:00:00 UTC",
+                "Discount End": "16-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b09091_81e1b86c9638fd74"
+   ] }, {
+        "Name": "Red Unite Style: Charizard + License",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Available Until": "06-03-2023 23:59:59 UTC",
+                "Gems": "695",
+                "Discounted Gems Price": "591",
+                "Discount Start": "27-02-2023 04:00:00 UTC",
+                "Discount End": "06-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0910_01ab03236ce79f96"
+   ] }, {
+        "Name": "Blue Unite Style(Sporty): Gardevoir + License",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Available Until": "06-03-2023 23:59:59 UTC",
+                "Gems": "810",
+                "Discounted Gems Price": "689",
+                "Discount Start": "27-02-2023 04:00:00 UTC",
+                "Discount End": "06-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0911_ec363060cc6692ab"
+   ] }, {
+        "Name": "Red Unite Style: Duraludon + License",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Available Until": "06-03-2023 23:59:59 UTC",
+                "Gems": "925",
+                "Discounted Gems Price": "786",
+                "Discount Start": "27-02-2023 04:00:00 UTC",
+                "Discount End": "06-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_b0912_302271ffe7626871"
+   ] }, {
+        "Name": "Pink Unite Style: Snorlax + License",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Available Until": "06-03-2023 23:59:59 UTC",
+                "Gems": "695",
+                "Discounted Gems Price": "591",
+                "Discount Start": "27-02-2023 04:00:00 UTC",
+                "Discount End": "06-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0913_05fbdbe979a3fa1e"
+   ] }, {
+        "Name": "Blue Unite Style(Active): Eldegoss + License",
+        "Info": {
+                "Release Date": "27-02-2023 04:00:00 UTC",
+                "Available Until": "06-03-2023 23:59:59 UTC",
+                "Gems": "695",
+                "Discounted Gems Price": "591",
+                "Discount Start": "27-02-2023 04:00:00 UTC",
+                "Discount End": "06-03-2023 23:59:59 UTC",
+            },
+        "type": ["t_combination_shop_pro_b0914_25d8b1ecab9745b3"]
+    }
+]
