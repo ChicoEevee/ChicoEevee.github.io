9c8079e6b8bc64b692240ce521c208493cd3fcae
ElChicoEevee <kirbyyeray@hotmail.com>
2024-01-23 12:51:42 +0100

    update~

--- Palworld/script2.js
+++ Palworld/script2.js
@@ -5,10 +5,12 @@ for (let badge of badges) {
 	let a = document.createElement('div')
 	a.classList.add('animal')
 	let t = `<p class="titlecolor"><strong>PARENTS</strong></p>`
+	t += `<p><strong></strong>${badge.ParentsName}</p>`
 	for (let x of badge.Parents) {
 		t += `<img src="./images/${x}.png">`
 	}	
 	t += `<p class="titlecolor"><strong>RESULT</strong></p>`
+	t += `<p><strong></strong>${badge.ChildName}</p>`
 	t += `<img src="./images/${badge.Child}.png">`
 	a.innerHTML = t
 	app.appendChild(a)
